e25904fe5d959308da20fbd9007206d0d7f8fa81
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2018-06-28 22:34:15 +0200

    dodajem primjer za linearnu slozenost

---
+++ linearna_slozenost.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env rvm-auto-ruby
+
+require 'benchmark'
+
+# Ovdje koristimo linearnu ili sekvencijalnu pretragu kao primjer.
+# Pretraga velikog niza sa 10 miliona mnogo će duže trajati od
+# pretrage malog niza od 1000 članova.
+
+def linear_search(array, value)
+  array.each { |i| return i if i == value }
+end
+
+mali_niz = (0..1000).to_a
+veliki_niz = (1..10_000_000).to_a
+
+Benchmark.bm(50) do |x|
+  x.report("linear_search(1,000)") { linear_search(mali_niz,1000) }
+  x.report("linear_search(10,000,000)") { linear_search(veliki_niz,1000000) }
+end
