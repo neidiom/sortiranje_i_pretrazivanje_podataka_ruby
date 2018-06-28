5b73cef88df926a99123f41118dec38ffa2d495e
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2018-06-28 22:33:01 +0200

    dodajem fajl konstantna slozenost

---
+++ konstantna_slozenost.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env rvm-auto-ruby
+
+require 'benchmark'
+
+# Primjer gdje dodajemo postojećem nizu novi član.
+# Možemo primjetiti da ma koliko je veliki niz vrijeme koje je potrebno
+# da se doda novi član postojećem nizu je uvijek isti.
+
+mali_niz = [1, 2, 3, 4, 5]
+veliki_niz = (1..9_000_000).to_a
+
+Benchmark.bm(10) do |x|
+  x.report("mali_niz_pristup_prvom_clanu") { mali_niz[0] }
+  x.report("veliki_niz_pristup_prvom_clanu") { veliki_niz[0] }
+end
