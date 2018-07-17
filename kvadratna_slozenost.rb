8c64f6b4aca99b38a34b25cb66b2701fd3ac6389
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2018-07-17 13:25:06 +0200

    dodajem kvadratnu slozenost

---
+++ kvadratna_slozenost.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env rvm-auto-ruby
+
+require 'benchmark'
+
+mali_niz_naizmjenicno_izmijesan = (1..10).to_a.shuffle
+veliki_niz_naizmjenicno_izmijesan = (1..10_000).to_a.shuffle
+
+# O(N^2)
+def bubble_sort(array)
+  n = array.length
+
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+
+  array
+end
+
+Benchmark.bm(50) do |x|
+  x.report("bubble_sort([1..10])") { bubble_sort(mali_niz_naizmjenicno_izmijesan) }
+  x.report("bubble_sort([1..10,000])") { bubble_sort(veliki_niz_naizmjenicno_izmijesan) }
+end
